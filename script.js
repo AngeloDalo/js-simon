3ec22cd6e6c21981be4c8f2c76093cfa0d0d7e82
AngeloDalo <angelo251158@gmail.com>
2021-12-03 13:45:45 +0100

    numeri casuali

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 //Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 //Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+const numeriCasuali = [];
+const TOT_NUMERI = 5;
+const MIN_NUMERO = 0;
+const MAX_NUMERO = 100;
+
+for (let i=0; i<TOT_NUMERI; i++) {
+    let numero = getRndInteger(MIN_NUMERO, MAX_NUMERO);
+    numeriCasuali.push(numero);
+}
+
+console.log (numeriCasuali);
