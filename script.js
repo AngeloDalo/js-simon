43fa381807e11272c2ef3fbaf9304fd5208cf152
AngeloDalo <angelo251158@gmail.com>
2021-12-03 14:16:09 +0100

    alert

--- script.js
+++ script.js
@@ -12,25 +12,42 @@ const TOT_NUMERI = 5;
 const MIN_NUMERO = 0;
 const MAX_NUMERO = 100;
 const container = document.querySelector(".container");
-//console.log(container);
 
+//creazione numeri casuali
 for (let i=0; i<TOT_NUMERI; i++) {
     let numero = getRndInteger(MIN_NUMERO, MAX_NUMERO);
     numeriCasuali.push(numero);
     container.innerHTML = container.innerHTML + numeriCasuali[i] + " ";
 }
 
+//pulizia numeri html
 setTimeout(function(){ 
         container.innerHTML = "";
 }, 3000);
 
+//inserimento numeri utente 
 setTimeout(function(){ 
     for (let i=0; i<TOT_NUMERI; i++)  {
         container.innerHTML = "";
         let numeroUtente = parseInt(prompt("Inserisci numero: "));
         numeriUtente.push(numeroUtente);
-    } 
-}, 4000);
+    }
 
-console.log (numeriUtente);
-//console.log (numeriCasuali);
+    let contatore = 0;
+    let numeriIndovinati = [];
+
+    //conta dei numeri indovinati
+    for (let i=0; i<TOT_NUMERI; i++) {
+        if (numeriUtente[i] == numeriCasuali [i]) {
+            numeriIndovinati.push(numeriCasuali[i]);
+            contatore = contatore + 1;
+        }
+    }
+
+    //vittoria solo se contatore è uguale al numero dei numeri da indovinare
+    if (contatore == TOT_NUMERI) {
+        alert ("HAI VINTO " + contatore + " NUMERI INDOVINATI: " + numeriIndovinati);
+    } else {
+        alert ("HAI PERSO " + contatore + " NUMERI INDOVINATI: " + numeriIndovinati);
+    }
+}, 3100);
